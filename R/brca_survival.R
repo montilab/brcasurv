91770d0d01d1c2e9985d297e05ab42ae4eb64c8c
Andrew Chen <andrewdchen@berkeley.edu>
2025-01-15 13:43:15 -0500

    Consolidate to one function

--- R/brca_survival.R
+++ R/brca_survival.R
@@ -1,130 +1,125 @@
 library(GSVA)
 library(Biobase)
 library(survival)
+library(here)
 
-tcga_gsva_data <- function(sigs_list, 
-                           adjust_prolif = TRUE,
-                           adjust_inflam = TRUE) {
-  
-  # Loading Data
-  tcga_data <- load(file.path("data", "tcga_data.rda"))
-  
+#' @title GSVA scoring on TCGA + METABRIC
+#' @description Processes gene set variation analysis (GSVA) data for breast cancer datasets.
+#' @param sigs_list A list of character vectors representing gene signatures.
+#' @param brca_data Character string specifying the breast cancer dataset to use. Options are "TCGA" or "METABRIC".
+#' @param adjust_prolif Logical indicating whether to adjust for proliferation signature.
+#' @param adjust_inflam Logical indicating whether to adjust for inflammation signature.
+#' @return A GSVA object containing processed data.
+#' @import GSVA Biobase survival here
+#' @export
+gsva_data <- function(sigs_list,
+                      brca_data = c("TCGA", "METABRIC"),
+                      adjust_prolif = TRUE,
+                      adjust_inflam = TRUE) {
+
+  brca_data <- match.arg(brca_data, c("TCGA", "METABRIC"))
+
+  # Assertion statements
+  stopifnot("sigs_list must be a list" = is.list(sigs_list))
+  stopifnot("elements of sigs_list must be char vectors" = is.character(sigs_list[[1]]))
+
+  # Loading signatures
   if (adjust_prolif) {
-    prolif_sig <- load(file.path("data", "prolif_sig.rda"))
+    load(here("data",  "prolif_sig.rda"))
     sigs_list <- c(sigs_list, prolif_sig)
   }
   if (adjust_inflam) {
-    inflam_sig <- load(file.path("data", "inflam_sig.rda"))
+    load(here("data",  "inflam_sig.rda"))
     sigs_list <- c(sigs_list, inflam_sig)
   }
-  
-  # Running GSVA
-  tcga_gsva <- GSVA::gsva(tcga_data, sigs_list, mx.diff=TRUE, verbose=FALSE)
-  
-  # Survival Filtering 
-  na_filter <- !is.na(tcga_gsva$vital_status)
-  missing_death_day_filter <- !((tcga_gsva$vital_status == "Dead") & is.na(tcga_gsva$days_to_death))
-  subtype_filter <- !is.na(tcga_gsva$subtype_selected)
-  data_filter <- na_filter & missing_death_day_filter & subtype_filter
-  
-  tcga_gsva <- tcga_gsva[, data_filter]
-  pData(tcga_gsva) <- pData(tcga_gsva) %>%
-    tibble::rownames_to_column("ID") %>%
-    dplyr::mutate(time = if_else(!is.na(days_to_death), days_to_death, days_to_last_follow_up)) %>%
-    dplyr::mutate(time_5 = if_else(as.numeric(time) < 1825.0, as.numeric(time), 1826.0)) %>%
-    dplyr::mutate(vital_status_1 = if_else(vital_status == "Alive", 1, 2)) %>%
-    dplyr::mutate(vital_status_5 = if_else(vital_status == "Dead" & (time_5 > 1825.0), 1, vital_status_1)) %>%
-    tibble::column_to_rownames("ID")
-  
-  return(tcga_gsva)
+
+  if (brca_data == "TCGA") {
+    load(here("data",  "tcga_data.rda"))
+    gsva_data <- GSVA::gsva(tcga_data, sigs_list, mx.diff=TRUE, verbose=FALSE)
+
+    # Survival Filtering
+    na_filter <- !is.na(gsva_data$vital_status)
+    missing_death_day_filter <- !((gsva_data$vital_status == "Dead") & is.na(gsva_data$days_to_death))
+    subtype_filter <- !is.na(gsva_data$subtype_selected)
+    data_filter <- na_filter & missing_death_day_filter & subtype_filter
+
+    gsva_data <- gsva_data[, data_filter]
+    pData(gsva_data) <- pData(gsva_data) %>%
+      tibble::rownames_to_column("ID") %>%
+      dplyr::mutate(time = if_else(!is.na(days_to_death), days_to_death, days_to_last_follow_up)) %>%
+      dplyr::mutate(time_5 = if_else(as.numeric(time) < 1825.0, as.numeric(time), 1826.0)) %>%
+      dplyr::mutate(vital_status_1 = if_else(vital_status == "Alive", 1, 2)) %>%
+      dplyr::mutate(vital_status_5 = if_else(vital_status == "Dead" & (time_5 > 1825.0), 1, vital_status_1)) %>%
+      tibble::column_to_rownames("ID")
+
+  } else if (brca_data == "METABRIC") {
+    load(here("data",  "metabric_data.rda"))
+    gsva_data <- GSVA::gsva(metabric_data, sigs_list, mx.diff=TRUE, verbose=FALSE)
+
+    pData(gsva_data) <- pData(gsva_data) %>%
+      dplyr::mutate(time = OS_MONTHS * 30.437) %>%
+      dplyr::mutate(time_5 = if_else(as.numeric(time) < 1825.0, as.numeric(time), 1826.0)) %>%
+      dplyr::mutate(vital_status_1 = if_else(OS_STATUS == "LIVING", 1, 2)) %>%
+      dplyr::mutate(vital_status_5 = if_else(OS_STATUS == "DECEASED" & (time_5 > 1825.0), 1, vital_status_1))
+  }
+
+  return(gsva_data)
 }
 
-tcga_cox_fit <- function(tcga_gsva,
+#' @title Cox Proportional Hazards Model Fitting for GSVA Data
+#' @description Fits Cox proportional hazards models to GSVA data for breast cancer datasets.
+#' @param gsva_data A GSVA object containing processed data.
+#' @param brca_data Character string specifying the breast cancer dataset used. Options are "TCGA" or "METABRIC".
+#' @param five_year Logical indicating whether to use 5-year survival data.
+#' @param adjust_age Logical indicating whether to adjust for age.
+#' @param adjust_prolif Logical indicating whether to adjust for proliferation signature.
+#' @param adjust_inflam Logical indicating whether to adjust for inflammation signature.
+#' @return A list of fitted Cox proportional hazards models.
+#' @import survival
+#' @export
+gsva_cox_fit <- function(gsva_data,
+                         brca_data = c("TCGA", "METABRIC"),
                          five_year = FALSE,
                          adjust_age = TRUE,
                          adjust_prolif = TRUE,
                          adjust_inflam = TRUE) {
-  
-  all_sigs <- rownames(tcga_gsva) 
-  all_sigs <- all_sigs[!(all_sigs %in% c("prolif", "inflam"))]
-  
+
+  brca_data <- match.arg(brca_data, c("TCGA", "METABRIC"))
+
+  all_sigs <- rownames(gsva_data)
+  exp_sigs <- all_sigs[!(all_sigs %in% c("prolif", "inflam"))]
+
   if(five_year) {
     surv_response <- "Surv(as.numeric(time_5), vital_status_5)"
   } else {
     surv_response <- "Surv(as.numeric(time), vital_status_1)"
   }
-  
-  covariate_flags <- c(adjust_age, adjust_prolif, adjust_inflam)
-  covariates <- c("age_at_index", "prolif", "inflam")[covariate_flags]
-  print(paste(c("Adjusting for:", covariates), collapse = " "))
-  
-  cox_fits <- list()
-  for (sig in all_sigs) {
-    # Adding gsva scores to pdata
-    tcga_gsva[[sig]] <- t(exprs(tcga_gsva[sig,]))
-    
-    # Fitting Cox model
-    cox_fits[[sig]] <- coxph(reformulate(c(covariates, sig), response = surv_response), data = pData(tcga_gsva))
-  }
-  
-  return(cox_fits)
-}
 
-metabric_gsva_data <- function(sigs_list, 
-                               adjust_prolif = TRUE,
-                               adjust_inflam = TRUE) {
-  
-  # Loading Data
-  metabric_data <- load(file.path("data", "metabric_data.rda"))
-  
   if (adjust_prolif) {
-    prolif_sig <- load(file.path("data", "prolif_sig.rda"))
-    sigs_list <- c(sigs_list, prolif_sig)
+    gsva_data[["prolif"]] <- t(exprs(gsva_data["prolif",]))
   }
   if (adjust_inflam) {
-    inflam_sig <- load(file.path("data", "inflam_sig.rda"))
-    sigs_list <- c(sigs_list, inflam_sig)
+    gsva_data[["inflam"]] <- t(exprs(gsva_data["inflam",]))
   }
-  
-  # Running GSVA
-  metabric_gsva <- GSVA::gsva(metabric_data, sigs_list, mx.diff=TRUE, verbose=FALSE)
 
-  pData(metabric_gsva) <- pData(metabric_gsva) %>%
-    dplyr::mutate(time = OS_MONTHS * 30.437) %>%
-    dplyr::mutate(time_5 = if_else(as.numeric(time) < 1825.0, as.numeric(time), 1826.0)) %>%
-    dplyr::mutate(vital_status_1 = if_else(OS_STATUS == "LIVING", 1, 2)) %>%
-    dplyr::mutate(vital_status_5 = if_else(OS_STATUS == "DECEASED" & (time_5 > 1825.0), 1, vital_status_1))
-  
-  return(metabric_gsva)
-}
-
-metabric_cox_fit <- function(metabric_gsva,
-                             five_year = FALSE,
-                             adjust_age = TRUE,
-                             adjust_prolif = TRUE,
-                             adjust_inflam = TRUE) {
-  
-  all_sigs <- rownames(metabric_gsva) 
-  all_sigs <- all_sigs[!(all_sigs %in% c("prolif", "inflam"))]
-  
-  if(five_year) {
-    surv_response <- "Surv(as.numeric(time_5), vital_status_5)"
-  } else {
-    surv_response <- "Surv(as.numeric(time), vital_status_1)"
+  if(brca_data == "TCGA") {
+    covariate_flags <- c(adjust_age, adjust_prolif, adjust_inflam)
+    covariates <- c("age_at_index", "prolif", "inflam")[covariate_flags]
+    print(paste(c("Adjusting for:", covariates), collapse = " "))
+  } else if (brca_data == "METABRIC") {
+    covariate_flags <- c(adjust_age, adjust_prolif, adjust_inflam)
+    covariates <- c("AGE_AT_DIAGNOSIS", "prolif", "inflam")[covariate_flags]
+    print(paste(c("Adjusting for:", covariates), collapse = " "))
   }
-  
-  covariate_flags <- c(adjust_age, adjust_prolif, adjust_inflam)
-  covariates <- c("AGE_AT_DIAGNOSIS", "prolif", "inflam")[covariate_flags]
-  print(paste(c("Adjusting for:", covariates), collapse = " "))
-  
+
   cox_fits <- list()
-  for (sig in all_sigs) {
+  for (sig in exp_sigs) {
     # Adding gsva scores to pdata
-    metabric_gsva[[sig]] <- t(exprs(metabric_gsva[sig,]))
-    
+    gsva_data[[sig]] <- t(exprs(gsva_data[sig,]))
+
     # Fitting Cox model
-    cox_fits[[sig]] <- coxph(reformulate(c(covariates, sig), response = surv_response), data = pData(metabric_gsva))
+    cox_fits[[sig]] <- coxph(reformulate(c(covariates, sig), response = surv_response), data = pData(gsva_data))
   }
-  
+
   return(cox_fits)
 }
--- README.md
+++ README.md
@@ -4,21 +4,15 @@ Helper functions to estimate the survival associations of user-provided gene sig
 
 ## Example Usage:
 
-Given a named list of gene sets e.g. list(GS1 = c("gene1", "gene2", "gene3")) one can score each sample in TCGA/METABRIC with `tcga_gsva` and use `tcga_cox_fit` to fit coxph models.
+Given a named list of gene sets e.g. list(GS1 = c("gene1", "gene2", "gene3")) one can score each sample in TCGA/METABRIC with `gsva_data` and use `gsva_cox_fit` to fit coxph models.
 
 ```
-tcga_gsva <- tcga_gsva_data(sigs_list = sigs_list, 
-                            adjust_prolif = TRUE, 
-                            adjust_inflam = TRUE)
+gsva_data <- gsva_data(sigs_list = sigs_list, 
+                       brca_data = "TCGA", # Or "METABRIC"
+                       adjust_prolif = TRUE, 
+                       adjust_inflam = TRUE)
 
-tcga_cox_fits <- tcga_cox_fit(tcga_gsva, 
-                              five_year = FALSE,
-                              adjust_age = TRUE,
-                              adjust_prolif = TRUE,
-                              adjust_inflam = TRUE)
-
-tcga_cox_fits_5 <- tcga_cox_fit(tcga_gsva, 
-                              five_year = TRUE,
+gsva_cox_fits <- gsva_cox_fit(gsva_data, 
                               adjust_age = TRUE,
                               adjust_prolif = TRUE,
                               adjust_inflam = TRUE)
